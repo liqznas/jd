000ae1b388cc9939f38a83bfdb6dac883058af3c
liqznas <57453782+liqznas@users.noreply.github.com>
2021-01-26 23:42:04 -0600

    Update diy.sh
    update

--- diy.sh
+++ diy.sh
@@ -2,4 +2,4 @@
 05 */2 * * * cd /jd/scripts && wget -O cfd.js https://raw.githubusercontent.com/moposmall/Script/main/Me/jx_cfd.js
 14 */2 * * * cd /jd/scripts && bash jd cfd now
 15 9 * * * cd /jd/scripts && bash jd jd_coupon now
-5 */1 * * * cd /jd/scripts && bash jd jd_immortal_answer
+5 * * * * cd /jd/scripts && bash jd jd_immortal_answer
