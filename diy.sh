5d7a603a4ef57412a0ddf29f1f1a152881a798ee
liqznas <57453782+liqznas@users.noreply.github.com>
2021-01-27 10:15:54 -0600

    Update diy.sh
    update

--- diy.sh
+++ diy.sh
@@ -3,4 +3,4 @@
 14 */2 * * * cd /jd/scripts && bash jd cfd now
 15 9 * * * cd /jd/scripts && bash jd jd_coupon now
 40 * * * * cd /jd/scripts && wget -O dati.js https://raw.githubusercontent.com/i-chenzhe/qx/main/jd_dati.js
-*/25 * * * * cd /jd/scripts && bash jd dati
+*/20 * * * * cd /jd/scripts && bash jd dati
